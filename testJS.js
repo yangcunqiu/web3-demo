3cff7d5525e888a2f769133d92679a899cccbef4
cqyang <cqyang@glp.com>
2023-04-06 16:23:23 +0800

    init

---
+++ testJS.js
@@ -0,0 +1,6 @@
+var Web3 = require('web3')
+
+const web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+
+web3.eth.getBalance("0x057A16A940A8030A1adA52A756810d74F9dc7003")
+.then(console.log);
